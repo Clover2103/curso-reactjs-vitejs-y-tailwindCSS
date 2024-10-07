f49b64b783517236516e0c0675cb6cbb00712d91
Cristian Rubio <cristianfrubio01@gmail.com>
2024-10-07 16:18:25 -0500

    Se agrega el componente de las cartas a implementar como la llamada a su api correpsondiente que contiene los datos

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from '../../Components/Layout';
+import { Card } from '../../Components/Card';
 
 function Home() {
+
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, []);
+
   return (
     <Layout className='bg-red-100'>
       Home
+      <div className='grid grid-cols-4 gap-3 w-full max-w-screen-lg'>
+        {
+          items?.map(item => (
+            <Card
+              key={item.id}
+              data={item}
+            />
+          ))
+        }
+      </div>
     </Layout>
   );
 };
